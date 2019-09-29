45b163a70508edcf17654be388d19079ff1e2e40
Leioy <13902303081@139.com>
2019-09-29 16:34:30 +0800

    add 第四个测试用例

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,8 @@
 import * as chai from 'chai'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
 import Promise from '../src/promise'
+chai.use(sinonChai)
 const assert = chai.assert
 describe('Promise',() => {
   it('是一个类',() => {
@@ -21,5 +24,10 @@ describe('Promise',() => {
   it('new Promise(fn)会生成一个对象，对象有then方法', () => {
     const promise = new Promise(() => {})
     assert.isFunction(promise.then)
+  })
+  it('new Promise(fn)中的fn立即执行', () => {
+    const fn = sinon.fake()
+    new Promise(fn)
+    assert(fn.called)
   })
 })
